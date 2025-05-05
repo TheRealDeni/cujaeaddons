6ae3f9cecc2ae5e216634ade38a91d9deb3365f7
Keilabg <keilabg05@gmail.com>
2025-05-05 00:42:59 -0400

    limpieza

--- elearning_cujae/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from .import survey_question
-from .import slide_slide
-from .import slide_channel
-from .import survey_survey
-from .import badge
-from .import survey_survey
-from .import survey_user
-
--- elearning_cujae/wizard/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import gen_invoice_success
